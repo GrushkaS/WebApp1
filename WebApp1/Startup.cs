c8280fe8c31f17455cd7a7efbb6936b670bb1172
Vadim <vadimdrach600@gmail.com>
2021-04-21 15:11:45 +0300

    First routing
    Added custom middleware to track installed data

--- WebApp1/Startup.cs
+++ WebApp1/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -53,15 +54,61 @@ namespace WebApp1
             //});
             #endregion
 
-            app.Map("/index", Index);
-            app.Map("/about", About);
+            #region test2
+            //app.Map("/index", Index);
+            //app.Map("/about", About);
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Page not found");
+            //}
+            //    );
+            #endregion
+
+
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Page not found");
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                Endpoint endpoint = context.GetEndpoint();
+
+                if(endpoint != null)
+                {
+                    var routePattern = (endpoint as Microsoft.AspNetCore.Routing.RouteEndpoint)?.RoutePattern?.RawText;
+
+                    Debug.WriteLine($"Endpoint Name: {endpoint.DisplayName}");
+                    Debug.WriteLine($"Route Pattern: {routePattern}");
+
+                    await next();
+                }
+                else
+                {
+                    Debug.WriteLine("Endpoint: null");                  
+                    await context.Response.WriteAsync("Endpoint is not defined");
+                }
             }
                 );
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/index", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Index!");
+                }
+                    );
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                }
+                    );
+
+            }
+                );
         }
 
         private static void Index(IApplicationBuilder app)
